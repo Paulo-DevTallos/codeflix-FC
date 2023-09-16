fe5e4cbba06ef173a1799317f6b9f2be322020cc
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-16 11:44:19 -0300

    test: ensure category receive a name

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,13 @@ import { Category } from "./Category"
 
 describe("Category Entity unit test", () => {
     it ("Should test constructor of category", () => {
-        const category = new Category("Movie")
+        const category = new Category({
+            name: 'Movie',
+            description: 'Qualquer descrição',
+            is_active: true,
+            created_at: new Date() 
+        });
 
-        expect(category.name).toBe("Movie");
+        expect(category.categoryProps.name).toBe("Movie");
     })
 })

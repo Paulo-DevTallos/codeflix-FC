ef19b905d434400b9bfbb01a913b890f301d0d21
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-16 13:57:40 -0300

    test: ensure strict equality to create to create a category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,7 +1,30 @@
 import { Category } from "./Category"
 import { omit } from "lodash";
 
-describe("Category Entity unit test", () => {
+describe("Category Entity constructor unit test", () => {
+    it ("Should test strict equality props to create a category", () => {
+        const sut = new Category({
+            name: "Movie",
+            description: "Some description",
+            is_active: true
+        });
+        let created_at = new Date();
+        
+        expect(sut.categoryProps).toStrictEqual({
+            name: "Movie",
+            description: "Some description",
+            is_active: true,
+            created_at,
+        });
+
+        expect(sut.categoryProps).not.toStrictEqual({
+            name: "Movie",
+            description: "Other description",
+            is_active: false,
+            created_at,
+        });
+    });
+    
     it ("Should test a creation of a category by name", () => {
         const sut = new Category({ name: 'Movie' });
         const props = omit(sut.categoryProps, 'created_at');
@@ -13,7 +36,14 @@ describe("Category Entity unit test", () => {
         });
     });
 
-    it ("Should test a creation of a category by name", () => {
+    it ("Should test if created_at is an instanceof Date", () => {
+        const sut = new Category({ name: 'Movie' });
+        const created_at = sut.categoryProps.created_at;
+        
+        expect(created_at).toBeInstanceOf(Date);
+    });
+
+    it ("Should test if created_at is an instanceof Date", () => {
         const sut = new Category({ name: 'Movie' });
         const created_at = sut.categoryProps.created_at;
         

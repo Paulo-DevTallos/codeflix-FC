8dce78fa17feb142652575270c6f5879c3427712
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-16 13:27:50 -0300

    feat: add private setter to optional logics

--- src/category/domain/entities/Category.ts
+++ src/category/domain/entities/Category.ts
@@ -2,10 +2,15 @@ interface CategoryProps {
     name: string;
     description?: string;
     is_active?: boolean;
-    created_at: Date;    
+    created_at?: Date;    
 }
 export class Category {
-    constructor(public readonly categoryProps: CategoryProps) {}
+    constructor(public readonly categoryProps: CategoryProps) {
+        /** tratando logica das props opcionais */
+        this.description = this.categoryProps.description;
+        this.is_active = this.categoryProps.is_active;
+        this.categoryProps.created_at = this.categoryProps.created_at ?? new Date();
+    }
 
     get name() {
         return this.categoryProps.name;
@@ -15,8 +20,16 @@ export class Category {
         return this.categoryProps.description;
     }
 
+    private set description(value: string) {
+        this.categoryProps.description = value ?? null;
+    }
+
     get is_active() {
         return this.categoryProps.is_active;
+    }
+
+    private set is_active(value: boolean) {
+        this.categoryProps.is_active = value ?? true;
     }
 
     get created_at() {
@@ -25,12 +38,6 @@ export class Category {
 }
 
 // create
-const category = new Category({ 
-    name: 'Movie',
-    description: 'Qualquer descrição',
-    is_active: true,
-    created_at: new Date() 
-})
-
+const category = new Category({ name: 'Movies' })
 
 

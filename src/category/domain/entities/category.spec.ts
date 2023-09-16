6d95eb947e81783d4e430672acc40ce73a7c2408
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-16 12:44:37 -0300

    test: ensure test object to strict equal compare

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,13 +2,26 @@ import { Category } from "./Category"
 
 describe("Category Entity unit test", () => {
     it ("Should test constructor of category", () => {
+        const created_at = new Date();
         const category = new Category({
-            name: 'Movie',
-            description: 'Qualquer descrição',
+            name: "Movie",
+            description: "Some description",
             is_active: true,
-            created_at: new Date() 
+            created_at,
         });
 
-        expect(category.categoryProps.name).toBe("Movie");
-    })
-})
+        expect(category.categoryProps).toStrictEqual({
+            name: "Movie",
+            description: "Some description",
+            is_active: true,
+            created_at,
+        });
+    });
+});
+
+/**
+ * 
+ * toMatchObject() - compara o objeto de forma parcial
+ * toStrictEqual() - garante igualdade
+ * 
+ */

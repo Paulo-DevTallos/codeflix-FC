2eb28c3dd30e7a88f6d5b16ae5dbadc3798268a3
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-10-13 15:16:41 -0300

    test: ensure validate method and UniqueUuid is instantiated

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidUuidError } from "./errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id.vo";
+
+describe('UniqueEntityId unit test', () => {
+    /**
+     * chamando a instancia de uma classe de Exception esperamos que ao 
+     * executar uma função com um id fake ela estoure a exception
+     */
+    it('sould test throw error when uuid is invalid', () => {
+        expect(() => new UniqueEntityId('fake id'))
+            .toThrow(new InvalidUuidError());
+    });
+
+    it('sould test if a validate method was called', () => {
+        const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+        expect(() => new UniqueEntityId('fake id'))
+            .toThrow(new InvalidUuidError());
+        expect(validateSpy).toHaveBeenCalled();
+    });
+});
